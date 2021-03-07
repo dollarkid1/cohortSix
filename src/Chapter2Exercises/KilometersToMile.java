f3e9dd879be0dd27aac0d5642a3abde1af0304f0
Humble Douglas <dollarkid17@gmail.com>
2021-03-08 00:51:40 +0100

    "working on text book exercises"

--- src/Chapter2Exercises/KilometersToMile.java
+++ src/Chapter2Exercises/KilometersToMile.java
@@ -9,11 +9,17 @@ public class KilometersToMile {
         while(true) {
 
             System.out.println("enter amount of KILOMETRES covered:");
+
             int miles = (int) (0.62137119 * input.nextInt());
+
             System.out.println("enter MINUTES taken:");
+
             double hour = (input.nextDouble() / 60);
+
             double averageSpeed = miles / hour;
+
             System.out.println("Average speed is " + averageSpeed + "mph");
+
             System.out.println();
         }
     }
