ad2a4f62aaf025ee1bc29088e1a0c7bd55139e26
Humble Douglas <dollarkid17@gmail.com>
2021-03-17 06:13:06 +0100

    "working on text book exercises"

---
+++ src/Chapter2Exercises/Array1.java
@@ -0,0 +1,11 @@
+package Chapter2Exercises;
+
+public class Array1 {
+    public static void main(String[] args) {
+        String[] AnimalsHunt = {"Lion", "Monkey", "Gazelles"};
+        String[] Tranquilizer = {"A12", "Pistol laser", "Sleep rifle"};
+        System.out.println("Welcome to Queen's Park Game Reserve! ");
+        System.out.println(AnimalsHunt[2]);
+        System.out.println(Tranquilizer.length);
+    }
+}
---
+++ src/Chapter2Exercises/Array2.java
@@ -0,0 +1,14 @@
+package Chapter2Exercises;
+
+public class Array2 {
+    public static void main(String[] args) {
+
+
+        int[] array = {17,9,23,4,3,2,45};
+        System.out.printf("%s%8s%n" ,"index" ,"value" );
+
+        for(int counter= 0; counter < array.length; counter++)
+        System.out.printf("%5d%8d%n", counter, array[counter]);
+
+    }
+}
